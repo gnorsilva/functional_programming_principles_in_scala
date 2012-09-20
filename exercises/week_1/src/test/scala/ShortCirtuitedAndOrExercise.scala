8f2d67266e01e647286efe068d0fbf0c09dc5552
Gonçalo Silva <gnorsilva@gmail.com>
2012-09-20 17:09:43 +0100

    adding notes for the first week and the first exercise as seen on lecture 4

---
+++ exercises/week_1/src/test/scala/ShortCirtuitedAndOrExercise.scala
@@ -0,0 +1,51 @@
+import org.scalatest.FunSuite
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+
+@RunWith(classOf[JUnitRunner])
+class ShortCirtuitedAndOrExercise extends FunSuite {
+
+  def shouldNotBeEvaluated = fail("this should not have been evaluated")
+
+  test("if both arguments are true 'and' is true") {
+    assert(and(true, true) === true)
+  }
+
+  test("if the first argument is false 'and' is false") {
+    assert(and(false, true) === false)
+  }
+
+  test("if the second argument is false 'and' is false") {
+    assert(and(true, false) === false)
+  }
+
+  test("if the first argument is false the second one is not evaluated") {
+    assert(and(false, shouldNotBeEvaluated) === false)
+  }
+
+  def and(x: Boolean, y: => Boolean): Boolean = {
+    if (!x) false else if(!y) false else true
+  }
+
+
+  test("if the first arguments is true 'or' is true") {
+    assert(or(true, false) === true)
+  }
+
+  test("if the second arguments is true 'or' is true") {
+    assert(or(false, true) === true)
+  }
+
+  test("if both arguments are false 'or' is false") {
+    assert(or(false, false) === false)
+  }
+
+  test("if the first argument is true the second one is not evaluated") {
+    assert(or(true, shouldNotBeEvaluated) === true)
+  }
+
+  def or(x: Boolean, y: => Boolean): Boolean = {
+    if (x) true else if (y) true else false
+  }
+
+}
